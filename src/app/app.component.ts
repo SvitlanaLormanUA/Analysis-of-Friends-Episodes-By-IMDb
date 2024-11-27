e77b404eca20759a79dee208deba7058a2ea9803
Svitlana Lorman <svlormanua@gmail.com>
2024-11-27 21:45:46 +0200

    did it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FlexmonsterPivot } from 'ngx-flexmonster';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flexmonster-project';
+  @ViewChild('pivot') pivotRef!: FlexmonsterPivot;
+
+  report = {
+    dataSource: {
+      browseForFile: true,
+      type: "csv",
+      fileName: "src/assets/friends_info.csv"
+    }
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.pivotRef) {
+      this.pivotRef.flexmonster.setReport(this.report);
+    }
+  }
+
+  customizeToolbar(toolbar: any) {
+    toolbar.showShareReportTab = true;
+  }
 }
